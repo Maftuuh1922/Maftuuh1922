b50ed43d699fa6bdeb203c2191bdbf1f224777fb
muhammad_maftuh <145763168+Maftuuh1922@users.noreply.github.com>
2025-06-28 02:30:30 +0700

    Update refresh-token.cjs

--- scripts/refresh-token.cjs
+++ scripts/refresh-token.cjs
@@ -1,13 +1,11 @@
-const fs = require('fs');
 const fetch = require('node-fetch');
-require('dotenv').config();
 
 const clientId = process.env.SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
 
 async function refreshAccessToken() {
-  const response = await fetch('https://accounts.spotify.com/api/token', {
+  const res = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
     headers: {
       Authorization: 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
@@ -19,11 +17,12 @@ async function refreshAccessToken() {
     }),
   });
 
-  const data = await response.json();
+  const data = await res.json();
+
   if (data.access_token) {
-    console.log('::set-output name=access_token::' + data.access_token);
+    console.log('access_token=' + data.access_token);
   } else {
-    console.error('Gagal refresh token:', data);
+    console.error('❌ Gagal refresh token:', data);
     process.exit(1);
   }
 }
